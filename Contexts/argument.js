fe9c590efb1de952f72f1100de3c96903ebcb22f
Marc Battesti <marcandriabattesti@gmail.com>
2018-12-17 21:11:44 +0100

    Function Contexts

---
+++ Contexts/argument.js
@@ -0,0 +1,17 @@
+function repeatThreeTimes(func) {
+  func();
+  func();
+  func();
+}
+
+var obj = {
+  a: 'hello',
+  b: 'world',
+  foo: function() {
+    [1, 2, 3].forEach(function(number) {
+      console.log(String(number) + ' ' + this.a + ' ' + this.b);
+    }, this);
+  },
+};
+
+obj.foo();
